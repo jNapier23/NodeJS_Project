dc64b79317f58eada3730d264c2242c59695d7c6
James Napier <jamesnapier@hod147037.home>
2023-03-16 17:00:02 +0000

    edited main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@
 # Create VPC with 3 public subnets each with 400+ IPs
 # Deployment instance with public HTTP access and MySQL
 
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+      }
+    }
+    required_version = ">= 1.2.0"
+}
+
+
 provider "aws" {
     access_key      = var.access_key
     secret_key      = var.secret_key
@@ -10,12 +21,20 @@ provider "aws" {
 
 resource "aws_instance" "ci_server" {
     ami             = "ami-0aaa5410833273cfe"    
-    instance_type   = "t2.small"
+    instance_type   = "t3.micro"
+
+    tags = {
+        Name = "CI_Server"
+    }
 }
 
 resource "aws_instance" "deployment" {
     ami             = "ami-0aaa5410833273cfe"
-    instance_type   = "t2.small"
+    instance_type   = "t3.micro"
+
+    tags = {
+        Name = "Deployment"
+    }
 }
 
 resource "aws_security_group" "ci_security" {
